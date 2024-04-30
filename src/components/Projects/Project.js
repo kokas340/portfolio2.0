c696b949a847f93a59b3d7db084567c1a5d6ccf9
Jack Spinola <jackspinola198@hotmail.com>
2024-04-30 21:47:38 +0200

    Deploy

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -9,40 +9,45 @@ function Project({
   buttonText,
   buttonLink,
   technologies,
+  button,
 }) {
   return (
     <div className="container">
-      <div className="row justify-content-center  ">
+      <div className="row justify-content-center h-75  ">
         {/* Left column for image */}
         <div className="col-xl-7 text-center">
           <img
             src={imageSrc}
             alt="Project Image"
             className="project-image"
-            style={{ width: "100%" }}
+            style={{ width: "110%" }}
           />
         </div>
         {/* Right column for title, subtitle, text, and button */}
         <div className="col-xl-5 pdlf d-flex align-items-center pbBig">
-          <div className="text-md-left">
+          <div className="text-md-left technologies">
             <h2 className="title mb-3">{title}</h2>
             {/* Mapping through technologies array to render squares */}
-            <div className="technologies">
+            <div className="technologies row">
               {technologies.map((tech, index) => (
-                <span key={index} className="tech-square ">
+                <span key={index} className="tech-square col text-center m-1">
                   {tech}
                 </span>
               ))}
             </div>
             <p className="project-text mt-3">{text}</p>
-            <a
-              href={buttonLink}
-              className="btn shadow-sm code-block"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {buttonText}
-            </a>
+            {button ? (
+              <a
+                href={buttonLink}
+                className="btn shadow-sm code-block"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {buttonText}
+              </a>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,21 +3,20 @@ import "./Projects.css";
 import Project from "./Project";
 import pc1 from "../../images/pc1.png";
 import pc2 from "../../images/pc2.png";
+import pnta from "../../images/pnta.png";
 
 function Projects() {
   return (
     <div className="container-fluid  container-fluid-pattern2">
-      <div className="container  " style={{ minHeight: "100vh" }}>
-        <div className="row justify-content-center pb-5">
+      <div className="container  ">
+        <div className=" justify-content-center pb-5">
           <div className="col-md-12 text-center mb-5">
             <p className="titleMain">PROJECTS 💻</p>
             <hr className="custom-hr" />
-            <div className="">
-              <p className="subtitle2">
-                Here, you'll discover some of the projects I've been involved
-                in. Additional projects will be showcased on GitHub.
-              </p>
-            </div>
+            <p className="subtitle2">
+              Here, you'll discover some of the projects I've been involved in.
+              Additional projects will be showcased on GitHub.
+            </p>
           </div>
           <Project
             imageSrc={pc2}
@@ -25,6 +24,7 @@ function Projects() {
             text="As the team leader for the cloud team, I spearheaded the development of an API connecting the Arduino from the IoT team and facilitating bidirectional data exchange with the frontend team."
             buttonText="View Project"
             buttonLink="https://github.com/Pet-Feeder-SEP4/PetFeeder"
+            button={true}
             technologies={[
               "Java",
               "Spring",
@@ -35,11 +35,32 @@ function Projects() {
             ]}
           />
           <Project
+            imageSrc={pnta}
+            title="Pnta 🪩"
+            text="
+         
+This app connects people across Denmark who enjoy bars and clubs. I designed and implemented the backend architecture and managed integration with the management platform. I also handled the website's design and implementation. "
+            buttonText="Comming soon"
+            button={false}
+            buttonLink=""
+            technologies={[
+              "Java",
+              "Spring",
+              "Websockets",
+              "PostgreSQL",
+              "TypeScript",
+              "React",
+              "Docker",
+              "Azure",
+            ]}
+          />
+          <Project
             imageSrc={pc1}
             title="Riga Cup ⚽"
             text="Responsible for crafting the backend infrastructure, I orchestrated the management of tournaments and teams within this system for Riga Cup"
             buttonText="Visit Website"
             buttonLink="https://www.rigacup.lv/"
+            button={true}
             technologies={["PHP", "SQL", "REACT"]}
           />
         </div>
