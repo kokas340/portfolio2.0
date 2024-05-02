fbde0cd6860d185c0f46ddda6aa38a1dd9253177
Jack Spinola <jackspinola198@hotmail.com>
2024-05-02 18:26:58 +0200

    About me section

---
+++ src/components/About/About.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import "./About.css";
+
+function About() {
+  const technologies = [
+    "Java",
+    "C#",
+    ".NET",
+    "SpringBoot",
+    "React",
+    "ReactNative",
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "CSS",
+    "Bootstrap",
+    "Tailwind",
+    "Azure",
+    "Docker",
+    "Kubernetes",
+    "PHP",
+    "CodeIgniter",
+    "Git",
+    "Github",
+  ];
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShow(window.innerWidth < 1200);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div className=" container-fluid-pattern2">
+      <div className="container">
+        <div className="row justify-content-center pb-5">
+          <div className="col-md-12 text-center mb-5">
+            <p className="titleMain">About Me 👨🏻‍💻</p>
+            <hr className="custom-hr mb-3" />
+            <p className="subtitle2">
+              Here you will find more information about me, what I do, and my
+              current skills mostly in terms of programming and technologies.
+            </p>
+          </div>
+        </div>
+        <div className="row justify-content-center pb-5">
+          <div className="col-md-6  ">
+            <p className="subtitleAbout">Get to know me! 🧑🏻</p>
+            <p className="w-100 experience-description mt-5">
+              I am a 21-year-old programmer currently living in Aarhus, Denmark.
+              I started programming in 2018 and am currently in my 5th semester
+              of Software Engineering. My objective is to obtain a challenging
+              role as a Software Engineer where I can apply my programming
+              skills and gain experience in the field. Let's grab a coffee
+              sometime and discuss technology!
+            </p>
+          </div>
+          <div className="col-xl-5 pdlf d-flex align-items-center ">
+            <div className="text-md-left technologies">
+              <p className="subtitleAbout">My Skills 📐</p>
+              {/* Mapping through technologies array to render squares */}
+              <div className="technologies row">
+                {technologies.map((tech, index) => (
+                  <span key={index} className="tech-square col text-center m-1">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
