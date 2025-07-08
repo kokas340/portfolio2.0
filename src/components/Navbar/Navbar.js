576eff0ad079bd13ad45dbb6b54235dda959e829
Jack Spinola <jackspinola198@hotmail.com>
2025-07-08 15:10:57 +0200

    zoom in on hero

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,12 +6,17 @@ function CustomNavbar() {
   return (
     <Navbar className="navbarcss" expand="lg">
       <Container>
+         {/*
         <Navbar.Brand className="custom-brand">Jack Spinola</Navbar.Brand>
+       
         <Nav className="ms-auto">
           <Nav.Link href="#projects">Projects</Nav.Link>
           <Nav.Link href="#education">Education</Nav.Link>
           <Nav.Link href="#about">About Me</Nav.Link>
         </Nav>
+      */}
+
+
       </Container>
     </Navbar>
   );
